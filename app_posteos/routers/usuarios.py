277a2a96b89fc4647f8d1e5fb5758ccf3ccf1ec6
ffrankoluka <fran.garcia.luca@gmail.com>
2026-04-09 23:15:41 -0300

    Carpeta Router para el logueo,creacion,etc de usuarios

--- app_posteos/routers/usuarios.py
+++ app_posteos/routers/usuarios.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter,Depends,HTTPException
+from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm import Session
 from app_posteos.database import get_db
 from app_posteos.models import Usuarios
+from app_posteos.schemas import UsuarioCreate,UsuarioResponse,UsuarioLogin
+from typing import Annotated
+
+
+router = APIRouter()
