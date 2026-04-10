f7bd4fe9b5a2a378e49dc33f0e5b517dc3b3dda3
ffrankoluka <fran.garcia.luca@gmail.com>
2026-04-09 23:03:47 -0300

    Agregamos Schemas y main para conexiones

---
+++ app_posteos/routers/usuarios.py
@@ -0,0 +1,5 @@
+from fastapi import APIRouter,Depends,HTTPException
+from sqlalchemy.orm import Session
+from app_posteos.database import get_db
+from app_posteos.models import Usuarios
+from 
---
+++ app_posteos/schemas.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+
+
+class UsuarioLogin(BaseModel):
+    email : str
+    contraseña : str
+    
+    
+class UsuarioCreate(BaseModel):
+    nombre : str
+    email : str
+    contraseña : str
+    
+    
+class UsuarioResponse(BaseModel):
+    user_id : int
+    nombre : str
+    email : str
+    
+    class Config:
+        from_attributes = True
+        
+        
+class posteoCreate(BaseModel):
+    title : str
+    contenido : str
+   
+    
+class posteoResponse(BaseModel):
+    id : int
+    title : str
+    contenido : str
+    created_at : str
+    user_id : int
+    
+    class Config:
+        from_attributes = True
+
