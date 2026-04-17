7bc032524c292896c1050981e8c7d5fa251b868a
ffrankoluka <fran.garcia.luca@gmail.com>
2026-04-17 11:39:40 -0300

    añadimos ruta usuarios completa

--- app_posteos/main.py
+++ app_posteos/main.py
@@ -1,4 +1,13 @@
-from fastapi import APIRouter
-
-
+from fastapi import FastAPI
 from app_posteos.routers import usuarios    
+from app_posteos.database import Base,engine
+from app_posteos import models
+
+
+
+app = FastAPI()
+
+app.include_router(usuarios.router)
+
+
+
