8174d761f62b3ddee2bb06a4616f609ed0dc8a75
ffrankoluka <fran.garcia.luca@gmail.com>
2026-04-30 11:36:26 -0300

    Agregamos Put en posteos

--- app_posteos/routers/posteos.py
+++ app_posteos/routers/posteos.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm import Session
 from app_posteos.database import get_db
 from app_posteos.models import Posteos,Usuarios
-from app_posteos.schemas import posteoCreate,posteoResponse
+from app_posteos.schemas import posteoCreate,posteoResponse,posteoPut,posteoDelete
 from typing import Annotated
 from app_posteos.routers.usuarios import get_current_user
 
@@ -27,4 +27,20 @@ async def get_posteo(user_id : int,db : Annotated[Session,Depends(get_db)]):
     if not posteo:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="No tiene Posteos registrados")
     return posteo
-   
+
+@router.put("/posteos/{post_id}",response_model=posteoResponse)
+async def update_posteo(post_id : int,posteo : posteoPut,db : Annotated[Session,Depends(get_db)],current_user : Annotated[Usuarios,Depends(get_current_user)]):
+    db_posteo = db.query(Posteos).filter(Posteos.post_id == post_id).first()
+    if not db_posteo:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Posteo no encontrado")
+    
+    if db_posteo.user_id != current_user.user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="No tienes permiso para modificar este posteo")
+    
+    db_posteo.title = posteo.title
+    db_posteo.contenido = posteo.contenido
+    db.commit()
+    db.refresh(db_posteo)
+    return db_posteo
+
+@router.delete("/posteos/{post_id}")
