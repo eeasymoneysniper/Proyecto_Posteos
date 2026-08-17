7c7d12c343b373ba181e536971a7fff4b6627f03
ffrankoluka <fran.garcia.luca@gmail.com>
2026-08-17 17:14:13 -0300

    MIDDLEWARE

--- PRACTICA/main.py
+++ PRACTICA/main.py
@@ -8,3 +8,5 @@ app.include_router(usuarios.router)
 app.include_router(lenguajes.router)
 app.include_router(auth.router)
 app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173"], allow_credentials=True, allow_methods=[""], allow_headers=[""])
+
+
